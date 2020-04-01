bce515265e22cf4d9a5e5885295a7e6096e087a4
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-04-01 21:16:51 +0300

    Update Store.java

--- Lab3/src/main/java/entities/Store.java
+++ Lab3/src/main/java/entities/Store.java
@@ -1,38 +1,61 @@
 package entities;
 
-import entities.Item;
-
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Store {
     private String name;
     private List<Item> items;
     private List<String> feedbackList;
 
-    public Store(String name, List<Item> items, List<String>feedbackList){
+    public Store(String name) {
         this.name = name;
-        this.items = items;
-        this.feedbackList = new ArrayList<String>();
+        this.items = new ArrayList<>();
+        this.feedbackList = new LinkedList<>();
     }
 
     public Store() {
+        super();
+        this.items = new ArrayList<>();
+        this.feedbackList = new LinkedList<>();
+    }
 
+    public void addFeedback(String feedback){
+        this.feedbackList.add(feedback);
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public List<Item> getItems() {
         return items;
+    }
+
+    public void setItems(Item item) {
+        this.items.add(item);
     }
 
     public List<String> getFeedbackList() {
         return feedbackList;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFeedbackList(List<String> feedbackList) {
+        this.feedbackList = feedbackList;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Store.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("items=" + items.size())
+                .add("feedbackList=" + feedbackList)
+                .toString();
     }
 }
