d8cd0d48e2f5cba21145ee1665b569fd93945e97
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-04-03 01:35:02 +0300

    add: case menu

--- Lab3/src/main/java/Main.java
+++ Lab3/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import Menu.CaseMenu;
+import entities.Item;
+import entities.Store;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import reader.Reader;
+import sorting.Sorting;
+
 import java.util.ArrayList;
-/*Создать стрим Item и:
+import java.util.Scanner;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/*Создать стрим entities.Item и:
         − проверить, есть ли товары, дороже 500;
         − Найти товары с максимальным и минимальным количеством единиц на складе
         − Отфильтровать товары с единственным магазином
@@ -7,29 +19,30 @@ import java.util.ArrayList;
         − Получить список магазинов
         − Напечатать информацию о магазинах, используя foreach
         − Получить список магазинов без дубликатов
-        a. ClassA - Item с полями:
+        a. ClassA - entities.Item с полями:
         o String name
         o Int price
         o Int count
-        o List<Store> stores
-        b. ClassB – Store с полями:
+        o List<entities.Store> stores
+        b. ClassB – entities.Store с полями:
         o String name
-        o List<Item> items
+        o List<entities.Item> items
         o List<String> feedbackList
 
  */
 public class Main {
+    static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         ArrayList<Item> items = new ArrayList<>();
         ArrayList<Store> stores = new ArrayList<>();
-    }
-    public static void printItem(Item item) {
-        System.out.println(item.getName() + " " + item.getPrice() + " " + item.getCount());
-        item.getStores().forEach(s -> System.out.println("  " + s.getItems() + " " + s.getFeedbackList() + s.getName()));
-    }
 
-    public static void printStore(Store store) {
-        System.out.println(store.getItems() + " " + store.getFeedbackList() + store.getName());
-        store.getItems().forEach(item -> System.out.println(item.getName() + " " + item.getPrice() + " " + item.getCount()));
+        Reader.readItems(items);
+        Reader.readStores(stores,items);
+
+        Supplier<Stream<Item>> itemSupply = items::stream;
+        Scanner in = new Scanner(System.in);
+
+        CaseMenu menu = new CaseMenu(in);
+        menu.start(itemSupply);
     }
 }
